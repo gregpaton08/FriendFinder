992908a8225fc496ea15383be3f31ef5b08110ff
Greg <gregpaton08@gmail.com>
2012-10-10 17:31:16 -0400

    [add] Location update working

--- src/com/gregpaton/friendfinder/MainActivity.java
+++ src/com/gregpaton/friendfinder/MainActivity.java
@@ -1,15 +1,58 @@
 package com.gregpaton.friendfinder;
 
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.os.Bundle;
 import android.app.Activity;
+import android.content.Context;
 import android.view.Menu;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends Activity {
+	
+	TextView _tvLocation;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        
+        _tvLocation = (TextView) findViewById(R.id.tvLocation);
+        
+        //Acquire a reference to the system Location Manager
+        LocationManager locationManager = (LocationManager) this.getSystemService(Context.LOCATION_SERVICE);
+
+        // Define a listener that responds to location updates
+        LocationListener locationListener = new LocationListener() {
+            public void onLocationChanged(Location location) {
+                // Called when a new location is found by the network location provider.
+                //makeUseOfNewLocation(location);
+
+        		String locationText = "Time: " + "Lat = " + location.getLatitude() + " Long = " + location.getLongitude() + "\n";
+
+            	
+            	if (location != null)
+            	{
+            		//Toast.makeText(getApplicationContext(), locationText, Toast.LENGTH_SHORT).show();
+            		_tvLocation.setText(locationText);
+            	}
+            }
+
+            public void onStatusChanged(String provider, int status, Bundle extras) {
+    	    }
+
+            public void onProviderEnabled(String provider) {
+            }
+
+            public void onProviderDisabled(String provider) {
+            }
+        };
+        // Register the listener with the Location Manager to receive location updates
+        locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, locationListener);
+        
+        
     }
 
     @Override
